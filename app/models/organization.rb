43bfb497942e51576d42fbd8064995a9c984ff75
t-townsend <tristatownsend@gmail.com>
2017-04-29 15:55:33 -0700

    added validations (#20)

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,6 +1,12 @@
 class Organization < ApplicationRecord
   has_one :manager
   has_one :user, through: :managers
+
   has_many :tags, through: :taggings
   has_many :taggings
+
+  validates :name, :address, :overview, :employees,
+            :teamsize, :website, :twitter, presence: true
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,10 @@ class User < ApplicationRecord
 
   has_one :manager
   has_one :organization, through: :managers
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  validates :name, :email, presence: true
+  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
