0fc97b719d3546b250bae84510ffe802988d85b3
Nic <nictamura@gmail.com>
2017-04-30 09:23:21 -0700

    Fix navbar stuff (#39)
    * fixed headers, added profile pic top nav, adjusted placemnt of profile dropdown, side nav links adjusted, icons adjusted, footer adjusted and ffilled content
    
    * fixed normal user dropdown, added placeholder pic. video parallax comments notes, minor adjustments on login/signup, added placeholder index files, changed parallax img, changed placements on all navs, added some syntax suga to header on parallax
    
    * added modals, cleaned up landing page
    
    * implement own user icon in nav, not done.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,6 +7,32 @@
 
 $(document).ready(function(){
 
-   $('.parallax').parallax();
-   $('.chosen-select').chosen()
+ $('.parallax').parallax();
+ $('.chosen-select').chosen();
+
+// modal config stuff
+ $('.modal').modal({
+     dismissible: true,
+     opacity: .2,
+     inDuration: 300,
+     outDuration: 200,
+     startingTop: '4%',
+     endingTop: '10%',
+   }
+ );
+
+// for user dropdown menu
+ $('.dropdown-button').dropdown({
+      inDuration: 500,
+      outDuration: 225,
+      constrainWidth: false,
+      hover: false,
+      gutter: -15,
+      belowOrigin: true,
+      alignment: 'right',
+      stopPropagation: false
+    }
+  );
+
+// all jQ goes above this closing brace
  });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
-  
+  def show
+
+  end
+
+  def edit
+  end 
+
 end
