d5e822c3f679e926ea4077827f6bdb04cf72f6b7
Josh Gamble <josh.gamble33@gmail.com>
2017-04-30 21:49:51 -0700

    api consumption (#85)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,19 +2,24 @@ require 'news_api'
 
 class ArticlesController < ApplicationController
   def index
+
+    Article.delete_all
+
     # an example of how to grab all the news stuff
     search_terms = NewsSearchTerm.all
 
     search_terms.each do |term|
       api = NewsApi.new
-      articles = api.articles('techcrunch')
+      articles = api.articles(term.name)
 
       # puts articles['articles']
 
       articles['articles'].each do |article|
-        @article = Article.new article
-        puts 'woo' if @article.save
+        article = Article.new article
+        puts 'woo' if article.save
       end
     end
+
+    @articles = Article.all
   end
 end
